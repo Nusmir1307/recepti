53539dac52c32cedf49766ff5b5b402d14c2227f
Nusmir1307 <Nusmir.Spahic@scv.si>
2025-01-02 18:53:34 +0100

    test

---
+++ city_insert.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once "db.php";
+
+$title = $_POST['title'];
+$post_number = $_POST['post_number'];
+
+$sql = "INSERT INTO cities (title, post_number) VALUES (?, ?)";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$title, $post_number]);
+
+//preusmeritev
+header("Location: cities.php");
+?>
